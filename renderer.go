ac360e6f624539b105baad4fbc0d9da0bf402f7b
Yota Toyama <raviqqe@gmail.com>
2024-01-11 18:22:31 +0900

    Fix

--- renderer.go
+++ renderer.go
@@ -207,9 +207,7 @@ func (r renderer) renderCells(cs []*messages.TableCell, ws []int) {
 func (renderer) escapeCellValue(c *messages.TableCell) string {
 	s := c.Value
 
-	s = strings.ReplaceAll(s, "\\\"", "\"")
 	s = strings.ReplaceAll(s, "\\", "\\\\")
-	s = strings.ReplaceAll(s, "\"", "\\\"")
 	s = strings.ReplaceAll(s, "\n", "\\n")
 
 	return s
