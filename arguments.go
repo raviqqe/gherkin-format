c6c08a4015d02beee23666cb3ca42bb60e9cc56b
Yota Toyama <raviqqe@gmail.com>
2025-08-02 13:53:23 -0700

    Refactor argument parsing (#52)

--- arguments.go
+++ arguments.go
@@ -12,17 +12,16 @@ type Arguments struct {
 }
 
 func GetArguments(ss []string) (Arguments, error) {
+	args := Arguments{}
 	s := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
-	c := s.Bool("check", false, "check if files are formatted correctly")
-	v := s.Bool("version", false, "show version information")
+
+	s.BoolVar(&args.Check, "check", false, "check if files are formatted correctly")
+	s.BoolVar(&args.Version, "version", false, "show version information")
 
 	if err := s.Parse(ss); err != nil {
 		return Arguments{}, err
 	}
 
-	return Arguments{
-		Check:   *c,
-		Paths:   s.Args(),
-		Version: *v,
-	}, nil
+	args.Paths = s.Args()
+	return args, nil
 }
